c09f0f43617f6cad84d3c61612a59c75461067f9
David Nebauer <david@nebauer.org>
2024-04-28 09:43:11 +0930

    modernise and remove Function::Parameters dependency

--- interactiveio-perl/source/lib/Dn/InteractiveIO.pm
+++ interactiveio-perl/source/lib/Dn/InteractiveIO.pm
@@ -1,81 +1,89 @@
 package Dn::InteractiveIO;
 
-use Moo;    #                                                          {{{1
+# modules    {{{1
+use Moo;
 use strictures 2;
-use 5.014_002;
-use version; our $VERSION = qv('0.1');
+use 5.006;
+use 5.038_001;
+use version; our $VERSION = qv('0.2');
 use namespace::clean;
 
-use Function::Parameters;
+use Carp qw(croak);
+use Const::Fast;
 use IO::Interactive;
 use IO::Prompter;
-use Readonly;
 use Term::ReadKey;
 
-Readonly my $TRUE  => 1;
-Readonly my $FALSE => 0;    #                                          }}}1
+const my $TRUE  => 1;
+const my $FALSE => 0;
 
-# Methods
+# }}}1
 
-# print($msg)                                                          {{{1
+# methods
+
+# print($msg)    {{{1
 #
 # does:   display ('print') message to stdout if connected to terminal
 # params: $msg - message [scalar string, optional]
 # prints: message to stdout
 # return: n/a, dies on failure
-method print ($msg) {
-    return if not $msg;
-    print {IO::Interactive::interactive} $msg;
-    return;
+sub print ($self, $msg)
+{    ## no critic (RequireInterpolationOfMetachars ProhibitBuiltinHomonyms)
+  return if not $msg;
+  print {IO::Interactive::interactive} $msg or croak;
+  return;
 }
 
-# say($msg)                                                            {{{1
+# say($msg)    {{{1
 #
 # does:   display ('say') message to stdout if connected to terminal
 # params: $msg - message [scalar string, optional]
 # prints: message to stdout
 # return: n/a, dies on failure
-method say ($msg) {
-    return if not $msg;
-    say {IO::Interactive::interactive} $msg;
-    return;
+sub say ($self, $msg)
+{ ## no critic (RequireInterpolationOfMetachars ProhibitBuiltinHomonyms ProhibitDuplicateLiteral)
+  return if not $msg;
+  say {IO::Interactive::interactive} $msg or croak;
+  return;
 }
 
-# warn($msg)                                                           {{{1
+# warn($msg)    {{{1
 #
 # does:   display ('say') message to stderr if connected to terminal
 # params: $msg - message [scalar string, optional]
 # prints: message to stderr
 # return: n/a, dies on failure
-method warn ($msg) {
-    return if not $msg;
-    say { IO::Interactive::interactive(*STDERR) } $msg;
-    return;
+sub warn ($self, $msg)
+{ ## no critic (RequireInterpolationOfMetachars ProhibitBuiltinHomonyms ProhibitDuplicateLiteral)
+  return if not $msg;
+  say { IO::Interactive::interactive(*STDERR) } $msg or croak;
+  return;
 }
 
-# die($msg)                                                            {{{1
+# die($msg)    {{{1
 #
 # does:   display ('say') message to stderr if connected to terminal
 #         and exit with failure exit status
 # params: $msg - message [scalar string, optional]
 # prints: message to stderr
 # return: n/a, dies on exit
-method die ($msg) {
-    if ($msg) { $self->warn($msg); }
-    exit 1;
+sub die ($self, $msg)
+{ ## no critic (RequireInterpolationOfMetachars ProhibitBuiltinHomonyms ProhibitDuplicateLiteral)
+  if ($msg) { $self->warn($msg); }
+  exit 1;
 }
 
-# interactive()                                                        {{{1
+# interactive()    {{{1
 #
 # does:   determine whether connected to terminal, i.e., is interactive
 # params: nil
 # prints: error message on failure
 # return: boolean
-method interactive () {
-    return IO::Interactive::is_interactive;
+sub interactive ($self) {    ## no critic (RequireInterpolationOfMetachars)
+  return IO::Interactive::is_interactive;
 }
 
-# confirm($question, $default)                                         {{{1
+# confirm($question, $default)    {{{1
 #
 # does:   user answers a yes-or-no question if connected to terminal
 # params: $question - question to answer
@@ -84,17 +92,18 @@ method interactive () {
 #                     [scalar boolean, optional, default=true]
 # prints: question and answer if interactive
 # return: boolean
-method confirm ($question = 'Proceed?', $default = $TRUE ) {
-    my $result = $default;
-    if (IO::Interactive::is_interactive) {
-        my @opts = ( '-yesno', '-single', '-echo' => 'Yes/No' );
-        $result = IO::Prompter::prompt( $question, @opts );
-    }
+sub confirm ($self, $question = 'Proceed?', $default = $TRUE)
+{    ## no critic (RequireInterpolationOfMetachars)
+  my $result = $default;
+  if (IO::Interactive::is_interactive) {
+    my @opts = ('-yesno', '-single', '-echo' => 'Yes/No');
+    $result = IO::Prompter::prompt($question, @opts);
+  }
 
-    return $result;
+  return $result;
 }
 
-# input($prompt, $default)                                             {{{1
+# input($prompt, $default)    {{{1
 #
 # does:   user enter a value if connected to terminal
 # params: $prompt  - user prompt
@@ -104,20 +113,21 @@ method confirm ($question = 'Proceed?', $default = $TRUE ) {
 # prints: prompt and input if interactive
 # return: scalar boolean
 # note:   empty input is allowed
-method input ($prompt = 'Enter value:', $default = ) {
-    my $input = $default;
-    if (IO::Interactive::is_interactive) {
+sub input ($self, $prompt = 'Enter value:', $default = $TRUE)
+{    ## no critic (RequireInterpolationOfMetachars)
+  my $input = $default;
+  if (IO::Interactive::is_interactive) {
 
-        # prompt() returns Contextual::Return object
-        # force scalar context with '.=' concatenation
-        my $value .= IO::Prompter::prompt($prompt);
-        $input = $value;
-    }
+    # prompt() returns Contextual::Return object
+    # force scalar context with '.=' concatenation
+    my $value = scalar IO::Prompter::prompt($prompt);
+    $input = $value;
+  }
 
-    return $input;
+  return $input;
 }
 
-# input_required($prompt, $default)                                    {{{1
+# input_required($prompt, $default)    {{{1
 #
 # does:   user enter a value if connected to terminal
 #         input is required
@@ -128,31 +138,36 @@ method input ($prompt = 'Enter value:', $default = ) {
 # prints: prompt and input if interactive
 # return: scalar boolean
 # note:   empty input is not accepted
-method input_required ($prompt = 'Enter value:', $default = ) {
-    my $input = $default;
-    if (IO::Interactive::is_interactive) {
-        while ($TRUE) {
+sub input_required ($self, $prompt = 'Enter value:', $default = $TRUE)
+{    ## no critic (RequireInterpolationOfMetachars ProhibitDuplicateLiteral)
+  my $input = $default;
+  if (IO::Interactive::is_interactive) {
+    while ($TRUE) {
 
-            # prompt() returns Contextual::Return object
-            # force scalar context with '.=' concatenation
-            $input = q{};
-            $input .= IO::Prompter::prompt($prompt);
-            last if $input;
-            $self->warn('Input is required - try again');
-        }
+      # prompt() returns Contextual::Return object
+      # force scalar context with '.=' concatenation
+      $input = q{};
+      $input .= IO::Prompter::prompt($prompt);
+      last if $input;
+      $self->warn('Input is required - try again');
     }
+  }
 
-    return $input;
-}    #                                                                 }}}1
+  return $input;
+}    # }}}1
 
 1;
 
-# POD                                                                  {{{1
+# POD    {{{1
 __END__
 
 =head1 NAME
 
 Dn::InteractiveIO - interactive methods
+
+=head1 VERSION
+
+This documentation applies to module Dn::InteractiveIO version 0.3.
 
 =head1 SYNOPSIS
 
@@ -183,11 +198,12 @@ Dn::InteractiveIO - interactive methods
 
 Displays interactive output, i.e., displays only when connected to a terminal.
 
-=head1 METHODS
+=head1 SUBROUTINES/METHODS
 
 =head2 print($msg)
 
-Sends message to stdout using C<print> command if connected to a terminal, i.e., environment is interactive.
+Sends message to stdout using C<print> command if connected to a terminal,
+i.e., environment is interactive.
 
 =head3 Parameters
 
@@ -201,7 +217,8 @@ Message to display. Scalar string. Optional.
 
 =head2 say($msg)
 
-Sends message to stdout using C<say> command if connected to a terminal, i.e., environment is interactive.
+Sends message to stdout using C<say> command if connected to a terminal,
+i.e., environment is interactive.
 
 =head3 Parameters
 
@@ -215,7 +232,8 @@ Message to display. Scalar string. Optional.
 
 =head2 warn($msg)
 
-Sends message to stderr using C<say> command if connected to a terminal, i.e., environment is interactive.
+Sends message to stderr using C<say> command if connected to a terminal,
+i.e., environment is interactive.
 
 =head3 Parameters
 
@@ -229,7 +247,8 @@ Message to display. Scalar string. Optional.
 
 =head2 die($msg)
 
-Sends message to stderr using C<say> command if connected to a terminal, i.e., environment is interactive. Then exits script with error code (1).
+Sends message to stderr using C<say> command if connected to a terminal,
+i.e., environment is interactive. Then exits script with error code (1).
 
 =head3 Parameters
 
@@ -263,11 +282,13 @@ User answers question in affirmative or negative.
 
 =item $question
 
-Question to display. Requires question mark. Scalar string. Optional. Default: 'Proceed?'
+Question to display. Requires question mark. Scalar string. Optional.
+Default: 'Proceed?'
 
 =item $default
 
-Value to return if not connected to terminal, i.e., not interactive. Scalar boolean. Optional. Default: true.
+Value to return if not connected to terminal, i.e., not interactive.
+Scalar boolean. Optional. Default: true.
 
 =back
 
@@ -277,7 +298,8 @@ Boolean.
 
 =head2 input($prompt, $default)
 
-User enters a value. An empty string, where the user simply presses the Enter key, is allowed.
+User enters a value. An empty string, where the user simply presses the
+Enter key, is allowed.
 
 =head3 Parameters
 
@@ -285,11 +307,13 @@ User enters a value. An empty string, where the user simply presses the Enter ke
 
 =item $prompt
 
-User prompt. Does not add colon automatically. Scalar string. Optional. Default: 'Enter value:'
+User prompt. Does not add colon automatically. Scalar string. Optional.
+Default: 'Enter value:'
 
 =item $default
 
-Value to return if not connected to terminal, i.e., not interactive. Scalar string. Optional. Default: undef.
+Value to return if not connected to terminal, i.e., not interactive.
+Scalar string. Optional. Default: undef.
 
 =back
 
@@ -299,7 +323,9 @@ Scalar string.
 
 =head2 input_required($prompt, $default)
 
-User enters a value. Note that empty input, where the user simply presses the Enter key, is not allowed. If the user enter an empty string an error message occurs and the user is presented again with the input prompt.
+User enters a value. Note that empty input, where the user simply presses the
+Enter key, is not allowed. If the user enter an empty string an error message
+occurs and the user is presented again with the input prompt.
 
 =head3 Parameters
 
@@ -307,11 +333,13 @@ User enters a value. Note that empty input, where the user simply presses the En
 
 =item $prompt
 
-User prompt. Does not add colon automatically. Scalar string. Optional. Default: 'Enter value:'
+User prompt. Does not add colon automatically. Scalar string. Optional.
+Default: 'Enter value:'
 
 =item $default
 
-Value to return if not connected to terminal, i.e., not interactive. Scalar string. Optional. Default: undef.
+Value to return if not connected to terminal, i.e., not interactive.
+Scalar string. Optional. Default: undef.
 
 =back
 
@@ -319,11 +347,27 @@ Value to return if not connected to terminal, i.e., not interactive. Scalar stri
 
 Scalar string. Note that an empty string is not permitted.
 
+=head1 DIAGNOSTICS
+
+=head2 Input is required - try again
+
+This warning is emitted if the user fails to enter input when input is
+required.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This module has no configuration files or environmental variables.
+
+=head1 INCOMPATIBILITIES
+
+There are no known incompatibilities.
+
 =head1 DEPENDENCIES
 
 =head2 Perl modules
 
-Function::Parameters, IO::Interactive, IO::Prompter, Moo, namespace::clean, Readonly, strictures, Term::ReadKey, version.
+Const::Fast, IO::Interactive, IO::Prompter, Moo, namespace::clean, strictures,
+Term::ReadKey, version.
 
 =head1 BUGS AND LIMITATIONS
 
@@ -342,4 +386,3 @@ it under the same terms as Perl itself.
 
 =cut
 # vim:fdm=marker
-
