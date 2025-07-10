e57758ccb42187024dae91c1041103d95798d827
David Nebauer <david@nebauer.org>
2025-07-10 21:49:16 +0930

    initial project import

---
+++ role-mp3-perl/source/README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Role::Mp3::Dn - Blah blah blah
+
+# SYNOPSIS
+
+    use Role::Mp3::Dn;
+
+# DESCRIPTION
+
+Role::Mp3::Dn is
+
+# AUTHOR
+
+David Nebauer <david@nebauer.org>
+
+# COPYRIGHT
+
+Copyright 2024- David Nebauer
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ role-mp3-perl/source/lib/Role/Mp3/Dn.pm
@@ -0,0 +1,39 @@
+package Role::Mp3::Dn;
+
+use strict;
+use 5.012;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Role::Mp3::Dn - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Role::Mp3::Dn;
+
+=head1 DESCRIPTION
+
+Role::Mp3::Dn is
+
+=head1 AUTHOR
+
+David Nebauer E<lt>david@nebauer.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2024- David Nebauer
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
